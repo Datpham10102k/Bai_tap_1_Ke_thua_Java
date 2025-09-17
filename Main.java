d75997acd884541838d5692ee82c89b6940cfb5e
Datpham10102k <phamthanhdat2468@gmail.com>
2025-09-17 11:13:14 +0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Kiểm thử lớp Circle
+        Circle circle = new Circle(2.5, "blue");
+        System.out.println(circle);
+
+        // Kiểm thử lớp Cylinder
+        Cylinder cylinder = new Cylinder(2.5, "green", 5.0);
+        System.out.println(cylinder);
+    }
+    }
